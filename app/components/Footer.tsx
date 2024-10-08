829bb32ffeda0f37b7738e28f5e983acfee5ef5f
Bastien Andre <bastienandre68@gmail.com>
2024-10-08 11:35:44 +0200

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Section } from "./Section"
+
+export const Footer = () => {
+    return (
+        <footer className="bg-card bg-black">
+            <Section className="py-8">
+                <p className="text-muted-foreground text-sm text-white">@Copywrite 2024 Bastien Andre</p>
+            </Section>
+        </footer>
+    )
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Section } from "./Section"
+
+export const Header = () => {
+    return(
+        <header className="sticky top-0">
+            <Section className="flex items-baseline">
+                <h1 className="text-4xl font-bold ">Bastien Andre</h1>
+            </Section>
+        </header>
+    )
+}
